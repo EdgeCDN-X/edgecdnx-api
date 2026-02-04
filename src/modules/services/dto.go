cdc4773997297cc4a96234d3ab3aff92ddd55b89
fr6nco <tomas.boros92@gmail.com>
2026-02-04 16:54:30 +0100

    Define service DTOs

--- src/modules/services/dto.go
+++ src/modules/services/dto.go
@@ -7,8 +7,35 @@ type StaticOriginDto struct {
 	Scheme     string `json:"scheme" binding:"required,oneof=http https"`
 }
 
+type S3OriginSpecDto struct {
+	AwsSigsVersion int    `json:"awsSigsVersion" binding:"required,oneof=2 4"`
+	S3AccessKeyId  string `json:"s3AccessKeyId" binding:"required"`
+	S3Region       string `json:"s3Region" binding:"required"`
+	S3SecretKey    string `json:"s3SecretKey" binding:"required"`
+	S3Server       string `json:"s3Server" binding:"required"`
+	S3ServerPort   int    `json:"s3ServerPort" binding:"required"`
+	S3ServerProto  string `json:"s3ServerProto" binding:"required,oneof=http https"`
+	S3Style        string `json:"s3Style" binding:"required,oneof=path virtual"`
+}
+
+type HostAliasDto struct {
+	Name string `json:"name" binding:"required,hostname"`
+}
+
+type CacheKeyDto struct {
+	Headers     []string `json:"headers,omitempty"`
+	QueryParams []string `json:"queryParams,omitempty"`
+}
+
 type ServiceDto struct {
 	Name         string           `json:"name" binding:"required,min=3,max=63"`
 	OriginType   string           `json:"originType" binding:"required,oneof=s3 static"`
 	StaticOrigin *StaticOriginDto `json:"staticOrigin,omitempty"`
+	S3OriginSpec *S3OriginSpecDto `json:"s3OriginSpec,omitempty"`
+	Cache        string           `json:"cache" binding:"required"`
+	HostAliases  []HostAliasDto   `json:"hostAliases,omitempty"`
+	CacheKey     *CacheKeyDto     `json:"cacheKey,omitempty"`
+
+	SignedUrlsEnabled bool `json:"signedUrlsEnabled"`
+	WafEnabled        bool `json:"wafEnabled"`
 }
